fa2d3f9e19c331cd884635f0c67761f4b8662693
Manuel <holamigos2015@gmail.com>
2020-11-22 18:44:40 +0100

    finito mvc

--- src/it/unibo/oop/lab/mvc/SimpleGUI.java
+++ src/it/unibo/oop/lab/mvc/SimpleGUI.java
@@ -1,9 +1,18 @@
 package it.unibo.oop.lab.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
  * A very simple program using a graphical interface.
@@ -12,6 +21,7 @@ import javax.swing.JFrame;
 public final class SimpleGUI {
 
     private final JFrame frame = new JFrame();
+    private final Controller controller;
 
     /*
      * Once the Controller is done, implement this class in such a way that:
@@ -38,7 +48,45 @@ public final class SimpleGUI {
      * builds a new {@link SimpleGUI}.
      */
     public SimpleGUI() {
-
+        controller = new ControllerImpl();
+        JPanel mainPanel = new JPanel(new BorderLayout());
+        frame.setContentPane(mainPanel);
+        JTextField textField1 = new JTextField();
+        JTextArea textArea1 = new JTextArea();
+        textArea1.setEnabled(false);
+        JButton printButton = new JButton("Print");
+        JButton historyButton = new JButton("Show History");
+        frame.add(textField1, BorderLayout.NORTH);
+        frame.add(textArea1, BorderLayout.CENTER);
+        JPanel secondaryPanel1 = new JPanel(new FlowLayout());
+        frame.add(secondaryPanel1, BorderLayout.SOUTH);
+        secondaryPanel1.add(printButton);
+        secondaryPanel1.add(historyButton);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        /*
+         * Handlers:
+         */
+        printButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                try {
+                    controller.setString(textField1.getText());
+                    controller.print();
+                } catch (IllegalArgumentException | NullPointerException f) {
+                    f.getMessage();
+                }
+            }
+        });
+        historyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                textArea1.setText("");
+                textArea1.append("Showing history:\n");
+                for (String s: controller.getHistory()) {
+                    textArea1.append(s + "\n");
+                }
+            }
+        });
         /*
          * Make the frame half the resolution of the screen. This very method is
          * enough for a single screen setup. In case of multiple monitors, the
@@ -60,6 +108,9 @@ public final class SimpleGUI {
          * on screen. Results may vary, but it is generally the best choice.
          */
         frame.setLocationByPlatform(true);
+        frame.setVisible(true);
     }
-
+        public static final void main(final String... args) {
+        new SimpleGUI();
+    }
 }
